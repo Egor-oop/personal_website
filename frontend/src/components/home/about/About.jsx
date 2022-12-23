18e83e09fb9340ab22bd14059bc8831f67c5dbda
Egor-oop <gulidoegor@gmail.com>
2022-12-23 19:46:51 +0400

    main-version branch

--- frontend/src/components/home/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const About = () => {
-  return (
-    <section id='about' className='code-text'>
-      I'm 13 years old. I'm learning progamming scince 10-11 years. I create sites, I write backend and frontend. I really like to write backend, cause a log of logic and a work with databases is one of my favorite part!
-    </section>
-  )
-}
