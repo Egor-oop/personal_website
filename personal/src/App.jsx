bfcab8d5e16683ecc8b66d3a80d756c34ba17754
Egor-oop <gulidoegor@gmail.com>
2022-12-29 17:36:23 +0300

    feature: toggle menu with links

--- personal/src/App.jsx
+++ personal/src/App.jsx
@@ -2,11 +2,13 @@ import { Main } from "./components/main/Main"
 import { Projects } from "./components/projects/Projects"
 import { Navigation } from "./components/navigatioin/Navigation"
 import { Skills } from "./components/skills/Skills"
+import { Toggle } from "./components/navigatioin/Toggle"
 
 function App() {
     return (
         <>
             <Navigation />
+            <Toggle />
             <Main />
             <Skills />
             {/* <Projects /> */}
--- personal/src/components/navigatioin/Toggle.jsx
+++ personal/src/components/navigatioin/Toggle.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Links } from './Links'
 import styles from './Toggle.module.scss'
 
 export const Toggle = () => {
+    const [isActive, setIsActive] = useState(false)
     return (
         <div className={styles['toggle']}>
-            <div className={styles['toggle__btn']}>
+            <div onClick={() => setIsActive(!isActive)} className={styles['toggle__btn']}>
                 <div />
                 <div />
                 <div />
+            </div>
+            <div
+                onClick={() => setIsActive(!isActive)}
+                className={`${styles['toggle__menu']} ${isActive ? styles['toggle__menu-active'] : styles['toggle__menu-inactive']}`} //${isActive ? 'active' : 'inactive'}
+            >
+                <div className={styles['toggle__links']}>
+                    <Links />
+                </div>
             </div>
         </div>
     )
